5b0fbce750469e857696e65137f5fe97c74e8e60
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-05-07 00:40:38 +0500

    try

--- airline_system/settings.py
+++ airline_system/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'flights.middleware.PromoCodeMiddleware'
+    'flights.middleware.PromoCodeMiddleware'
 ]
 
 CORS_ALLOWED_ORIGINS = [
--- flights/middleware.py
+++ flights/middleware.py
@@ -1,31 +1,22 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import random
 import string
 
-class PromoCodeMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+def generate_promo_code(length=6):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
-    def __call__(self, request):
-        # Пропускаем favicon и test
-        if request.path in ['/favicon.ico', '/api/test/']:
-            return self.get_response(request)
+@api_view(['GET'])
+def test_api(request):
+    is_new_user = request.COOKIES.get('new_user', 'true') == 'true'
+    promo_code = generate_promo_code() if is_new_user else None
 
-        is_new_user = request.COOKIES.get('new_user', 'true') == 'true'
-        promo_code = self.generate_promo_code() if is_new_user else None
+    response_data = {"message": "API is working"}
+    if promo_code:
+        response_data["promo_code"] = promo_code
 
-        response = self.get_response(request)
+    response = Response(response_data)
+    if promo_code:
+        response.set_cookie('new_user', 'false', max_age=30 * 24 * 60 * 60)
 
-        # Проверяем, есть ли у ответа .data и можно ли модифицировать
-        if promo_code and hasattr(response, 'data') and isinstance(response.data, dict):
-            try:
-                response.data['promo_code'] = promo_code
-                response._is_rendered = False
-                response.render()
-                response.set_cookie('new_user', 'false', max_age=30 * 24 * 60 * 60)
-            except Exception as e:
-                print("Middleware error:", e)
-
-        return response
-
-    def generate_promo_code(self, length=6):
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    return response

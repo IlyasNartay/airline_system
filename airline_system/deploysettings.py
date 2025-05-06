aa1af93763a2f7ce59badd7836d58ddd167bc173
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-05-07 02:12:23 +0500

    update deploySettings.py

--- airline_system/deploysettings.py
+++ airline_system/deploysettings.py
@@ -4,9 +4,17 @@ import os
 DEBUG = False
 
 hostname = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+DEFAULT_ALLOWED = [
+    "airline-system-pbjy.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
+
 if hostname:
-    ALLOWED_HOSTS = [hostname]
+    ALLOWED_HOSTS = [hostname] + DEFAULT_ALLOWED
     CSRF_TRUSTED_ORIGINS = ['https://' + hostname]
 else:
-    ALLOWED_HOSTS = []
-    CSRF_TRUSTED_ORIGINS = []
+    ALLOWED_HOSTS = DEFAULT_ALLOWED
+    CSRF_TRUSTED_ORIGINS = ['https://' + host for host in DEFAULT_ALLOWED if not host.startswith("127.")]
+

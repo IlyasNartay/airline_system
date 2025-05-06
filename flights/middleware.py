5f59630f1117780d8b1cdec595aab9aeaf976c6a
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-05-07 00:30:39 +0500

    test

--- flights/middleware.py
+++ flights/middleware.py
@@ -1,22 +1,29 @@
 import random
 import string
 
-from rest_framework.response import Response
-
 class PromoCodeMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # Пропускаем favicon и test
+        if request.path in ['/favicon.ico', '/api/test/']:
+            return self.get_response(request)
+
         is_new_user = request.COOKIES.get('new_user', 'true') == 'true'
         promo_code = self.generate_promo_code() if is_new_user else None
 
         response = self.get_response(request)
 
-        if promo_code and isinstance(response, Response):
-            if isinstance(response.data, dict):
+        # Проверяем, есть ли у ответа .data и можно ли модифицировать
+        if promo_code and hasattr(response, 'data') and isinstance(response.data, dict):
+            try:
                 response.data['promo_code'] = promo_code
+                response._is_rendered = False
+                response.render()
                 response.set_cookie('new_user', 'false', max_age=30 * 24 * 60 * 60)
+            except Exception as e:
+                print("Middleware error:", e)
 
         return response
 

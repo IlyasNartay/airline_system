423f4f5bb998958131067be2e997ee3a644c941f
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-05-07 14:56:03 +0500

    index ''

--- flights/urls.py
+++ flights/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterAPIView.as_view(), name='register'),
-    path('home', views.IndexAPIView.as_view(), name='index'),
+    path('', views.IndexAPIView.as_view(), name='index'),
     path('flight/<int:flight_id>/', views.FlightDetailAPIView.as_view(), name='flight_detail'),
     path('airport/<str:airport_code>/', views.AirportDetailAPIView.as_view(), name='airport_detail'),
     path('flight/bookings/', views.BookFlightAPIView.as_view(), name='book_flight'),
